86d75ab8562eddd63aaa9f330024f47ea6ed5cd2
magicliang <liangchuan@meituan.com>
2021-12-27 19:17:13 +0800

    init project

---
+++ src/test/java/com/magicliang/domaindriventransactionsys/DomainDrivenTransactionSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magicliang.domaindriventransactionsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainDrivenTransactionSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
