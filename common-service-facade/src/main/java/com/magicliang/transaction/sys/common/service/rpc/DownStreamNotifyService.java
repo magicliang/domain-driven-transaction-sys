cd544baf4c4d6e5f3d06d67477dee448f84959ea
magicliang <liangchuan@meituan.com>
2022-01-05 19:39:47 +0800

    完成所有的 rpc 的实现

---
+++ common-service-facade/src/main/java/com/magicliang/transaction/sys/common/service/rpc/DownStreamNotifyService.java
@@ -0,0 +1,78 @@
+package com.magicliang.transaction.sys.common.service.rpc;
+
+import com.magicliang.transaction.sys.biz.service.impl.request.callback.CallbackCommand;
+import com.magicliang.transaction.sys.common.service.facade.ICallbackFacade;
+import com.magicliang.transaction.sys.core.model.context.TransactionModel;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * project name: domain-driven-transaction-sys
+ * <p>
+ * description: 下游支付通道回调接口
+ *
+ * @author magicliang
+ * <p>
+ * date: 2022-01-05 19:19
+ */
+@Slf4j
+@Service("downStreamNotifyService")
+public class DownStreamNotifyService {
+
+    /**
+     * 回调返回值：成功
+     */
+    private static final String NOTIFY_SUCCESS_FLAG = "T";
+
+    /**
+     * 回调返回值：失败
+     */
+    private static final String NOTIFY_FAIL_FLAG = "F";
+
+    /**
+     * 回调门面
+     */
+    @Autowired
+    private ICallbackFacade callbackFacade;
+
+    /**
+     * 代付结果回调接口
+     *
+     * @param req 回调请求
+     * @return 回调处理结果
+     */
+    public Object paymentAgentNotify(Object req) {
+        Object resTo = new Object();
+        try {
+            // TODO：验签
+            CallbackCommand callbackCommand = buildCallbackCommand(req);
+            TransactionModel transactionModel = callbackFacade.callback(callbackCommand);
+
+            if (transactionModel.isSuccess()) {
+//                resTo.setData(NOTIFY_SUCCESS_FLAG);
+            } else {
+//                resTo.setData(NOTIFY_FAIL_FLAG);
+            }
+
+        } catch (Exception ex) {
+            log.error("下游支付通道回调异常，请求参数：{}", req, ex);
+//            resTo.setData(NOTIFY_FAIL_FLAG);
+        } finally {
+            log.info("下游支付通道回调，请求参数：{}，响应结果：{}", req, resTo);
+        }
+        return resTo;
+    }
+
+    /**
+     * 从回调请求里构造回调命令
+     *
+     * @param req 回调请求
+     * @return 回调命令
+     */
+    private CallbackCommand buildCallbackCommand(final Object req) {
+        CallbackCommand callbackCommand = new CallbackCommand();
+        // 构建基础回调请求
+        return callbackCommand;
+    }
+}
---
+++ common-service-facade/src/main/java/com/magicliang/transaction/sys/common/service/rpc/PayChannelServiceImpl.java
@@ -0,0 +1,79 @@
+package com.magicliang.transaction.sys.common.service.rpc;
+
+import com.magicliang.transaction.sys.biz.service.impl.request.acceptance.AlipayAcceptanceCommand;
+import com.magicliang.transaction.sys.biz.service.impl.request.payment.convertor.PaymentCommandConvertor;
+import com.magicliang.transaction.sys.common.service.facade.IAcceptanceFacade;
+import com.magicliang.transaction.sys.common.service.facade.IPaymentFacade;
+import com.magicliang.transaction.sys.core.model.context.TransactionModel;
+import com.magicliang.transaction.sys.core.model.entity.TransPayOrderEntity;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * project name: domain-driven-transaction-sys
+ * <p>
+ * description: 支付通道支付服务
+ *
+ * @author magicliang
+ * <p>
+ * date: 2022-01-05 19:25
+ */
+@Slf4j
+@Service("payChannelService")
+public class PayChannelServiceImpl {
+
+    /**
+     * 受理门面
+     */
+    @Autowired
+    private IAcceptanceFacade acceptanceFacade;
+
+    /**
+     * 支付门面
+     */
+    @Autowired
+    private IPaymentFacade paymentFacade;
+
+    /**
+     * 支付到支付宝余额
+     *
+     * @param payToAlipayRequest 原始 rpc 请求
+     * @return rpc 结果
+     */
+    public Object payToAlipay(final Object payToAlipayRequest) {
+        AlipayAcceptanceCommand command = convertToCommand(payToAlipayRequest);
+
+        TransactionModel transactionModel = acceptanceFacade.acceptPayOrder(command);
+
+        Object response = new Object();
+//        response.setBizIdentifyNo(payToAlipayRequest.getBizIdentifyNo());
+//        response.setBizUniqueNo(payToAlipayRequest.getBizUniqueNo());
+
+        if (transactionModel.isSuccess()) {
+//            response.setSuccess(true);
+//            response.setIdempotent(transactionModel.isIdempotent());
+            final TransPayOrderEntity payOrder = transactionModel.getPayOrder();
+            Long payOrderNo = payOrder.getPayOrderNo();
+//            response.setPayOrderNo(payOrderNo);
+
+            // 受理即异步支付
+            paymentFacade.asyncPay(PaymentCommandConvertor.fromDomainEntity(payOrder));
+        } else {
+//            response.setErrorCode(transactionModel.getErrorCode());
+//            response.setErrorMsg(transactionModel.getErrorMsg());
+        }
+        return response;
+    }
+
+    /**
+     * 转化 rpc 请求为门面命令
+     *
+     * @param payToAlipayRequest 原始 rpc 请求
+     * @return 门面命令
+     */
+    private AlipayAcceptanceCommand convertToCommand(final Object payToAlipayRequest) {
+        AlipayAcceptanceCommand alipayAcceptanceCommand = new AlipayAcceptanceCommand();
+        return alipayAcceptanceCommand;
+    }
+}
