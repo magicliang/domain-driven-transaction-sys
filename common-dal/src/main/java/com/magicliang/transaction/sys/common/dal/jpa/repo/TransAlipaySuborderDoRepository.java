e44180233710733d371f898b3feca2a700be26d0
magicliang <liangchuan@meituan.com>
2022-01-27 15:13:58 +0800

    增加 jpa 的依赖

--- common-dal/src/main/java/com/magicliang/transaction/sys/common/dal/jpa/repo/TransAlipaySuborderDoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.magicliang.transaction.sys.common.dal.jpa.repo;
-
-import com.magicliang.transaction.sys.common.dal.jpa.dataObject.TransAlipaySuborderDo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface TransAlipaySuborderDoRepository extends JpaRepository<TransAlipaySuborderDo, Long>, JpaSpecificationExecutor<TransAlipaySuborderDo> {
-}
--- common-dal/src/main/java/com/magicliang/transaction/sys/common/dal/jpa/repo/TransBankCardSuborderDoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.magicliang.transaction.sys.common.dal.jpa.repo;
-
-import com.magicliang.transaction.sys.common.dal.jpa.dataObject.TransBankCardSuborderDo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface TransBankCardSuborderDoRepository extends JpaRepository<TransBankCardSuborderDo, Long>, JpaSpecificationExecutor<TransBankCardSuborderDo> {
-}
