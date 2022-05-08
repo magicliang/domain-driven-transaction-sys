7708e5e1d1de046d2510ba94c0077773e48f308a
chuanliang <chuanliang@tencent.com>
2022-05-08 18:08:05 +0800

    增加 package-info.java

---
+++ common-util/src/main/java/com/magicliang/transaction/sys/common/aop/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * project name: domain-driven-transaction-sys
+ * <p>
+ * description:
+ * TODO：待实现支持所有 Spring 场景的 advice
+ *
+ * @author magicliang
+ * <p>
+ * date: 2022-05-08 18:07
+ */
+package com.magicliang.transaction.sys.common.aop;
