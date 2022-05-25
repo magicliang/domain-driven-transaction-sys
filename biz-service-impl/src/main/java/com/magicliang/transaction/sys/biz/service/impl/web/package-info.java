7a9e361aa5b348deec004e2dd66e533f15e8b3c9
chuanliang <chuanliang@tencent.com>
2022-05-25 13:58:27 +0800

    增加 web 包

---
+++ biz-service-impl/src/main/java/com/magicliang/transaction/sys/biz/service/impl/web/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * project name: domain-driven-transaction-sys
+ * <p>
+ * description: FIXME：使用 JWT 和 oAuth
+ *
+ * @author magicliang
+ * <p>
+ * date: 2022-05-25 13:57
+ */
+package com.magicliang.transaction.sys.biz.service.impl.web;
