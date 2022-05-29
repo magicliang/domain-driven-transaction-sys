5e375d1f322edbe4248b0864005d648710525469
chuanliang <chuanliang@tencent.com>
2022-05-29 12:35:39 +0800

    增加关于 aop 的测试

--- biz-service-impl/src/test/java/com/magicliang/transaction/sys/aop/factory/ProxyFactorTest.java
+++ biz-service-impl/src/test/java/com/magicliang/transaction/sys/aop/factory/ProxyFactorTest.java
@@ -30,14 +30,29 @@ class ProxyFactorTest extends DomainDrivenTransactionSysApplicationIntegrationTe
      * 测试平凡的代理工厂
      */
     @Test
-    public void testNormalProxyFactory() {
-        ProxyFactory factory = new ProxyFactory(integerGenerator);
+    public void testJdkDynamicProxyFactory() {
+        // 使用 HotSwappableTargetSource 必须添加接口
+        ProxyFactory factory = new ProxyFactory(HotSwappableTargetSourceFactory.createHotSwappableTargetSource(integerGenerator));
+        factory.setInterfaces(IntegerGenerator.class);
+        // 使用平凡的 api 则不用这样做，但底层的 advised 的 interfaces 成员里仍然有这个接口
+//        ProxyFactory factory = new ProxyFactory(integerGenerator);
+
         factory.addAdvice(new LoggingAdvice());
 //        factory.addAdvisor(myAdvisor);
-        final IntegerGenerator proxy = (IntegerGenerator) factory.getProxy();
+        // 如果底层实现是 JdkDynamicAopProxy，这个 object 是个合成的 Proxy 内部类，有个 h 成员为 JdkDynamicAopProxy，factory 能够识别出几个接口，底层的 realProxy 就能做怎样的转化
+        final Object realProxy = factory.getProxy();
+        final IntegerGenerator proxy = (IntegerGenerator) realProxy;
+        /*
+         * JdkDynamicAopProxy 是个包外不可见的类，所以不能再做 casting，所以只能用调试来一探究竟：
+         *
+         *
+         */
         log.info("proxy is: {}", proxy);
-        (JdkDynamicAopProxy) proxy;
         Assertions.assertTrue(true);
+
+        // 确认 bean parser 的加载过程
+        // 区别两个 MethodInterceptor
+        // 搞清楚最深层的 getProxy 的功能
     }
 
 }
--- core-service/src/main/java/com/magicliang/transaction/sys/aop/factory/HotSwappableTargetSourceFactory.java
+++ core-service/src/main/java/com/magicliang/transaction/sys/aop/factory/HotSwappableTargetSourceFactory.java
@@ -15,6 +15,8 @@ public class HotSwappableTargetSourceFactory {
 
     /**
      * 把原始对象包装成 HotSwappableTargetSource
+     * 这个 bean 本身也可以被 Spring 管理起来，也就是说 decorator 模式本身也可以在内部再做一次切换的，这点很重要
+     * 这样生成的 source 会使用 target 接口掩盖原生接口，除非 factory 另外 addInterfaces
      *
      * @param sourceTarget 原始对象
      * @return HotSwappableTargetSource
