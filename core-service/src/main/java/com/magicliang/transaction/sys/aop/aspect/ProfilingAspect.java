302bfd24cdee35ecbe38c780bc234a62ba89bc51
chuanliang <chuanliang@tencent.com>
2022-06-04 15:03:38 +0800

    增加第一个自定义的 Aspect

---
+++ core-service/src/main/java/com/magicliang/transaction/sys/aop/aspect/ProfilingAspect.java
@@ -0,0 +1,40 @@
+package com.magicliang.transaction.sys.aop.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+/**
+ * project name: domain-driven-transaction-sys
+ * <p>
+ * description:
+ *
+ * @author magicliang
+ * <p>
+ * date: 2022-06-04 13:54
+ */
+@Slf4j
+@Aspect
+@Component
+public class ProfilingAspect {
+
+    @Around("methodsToBeProfiled()")
+    public Object profile(ProceedingJoinPoint pjp) throws Throwable {
+        StopWatch sw = new StopWatch(getClass().getSimpleName());
+        try {
+            sw.start(pjp.getSignature().getName());
+            return pjp.proceed();
+        } finally {
+            sw.stop();
+            log.info(sw.prettyPrint());
+        }
+    }
+
+    @Pointcut("within(com.magicliang.transaction.sys..*)")
+    public void methodsToBeProfiled() {
+    }
+}
