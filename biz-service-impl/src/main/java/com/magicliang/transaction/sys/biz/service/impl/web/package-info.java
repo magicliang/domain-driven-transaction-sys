fb1ada1f94342b9445501a4dcb98e8b2f2558382
chuanliang <chuanliang@tencent.com>
2022-11-01 00:03:49 +0800

    增加注释

--- biz-service-impl/src/main/java/com/magicliang/transaction/sys/biz/service/impl/web/package-info.java
+++ biz-service-impl/src/main/java/com/magicliang/transaction/sys/biz/service/impl/web/package-info.java
@@ -2,6 +2,11 @@
  * project name: domain-driven-transaction-sys
  * <p>
  * description: FIXME：使用 JWT 和 oAuth
+ * 在这一层使用的 dto 要满足如下特征：
+ * <p>
+ * 1. immutable：使用 final + 构造器 + builder 模式赋值
+ * 2. 只是领域模型的片段
+ * 3. 贫血模型：no behavior
  *
  * @author magicliang
  * <p>
