daaba4f8347ac9977e6b8be83b51029166641f1c
chuanliang <chuanliang@tencent.com>
2022-11-17 18:50:27 +0800

    增加 controller advice

---
+++ biz-service-impl/src/main/java/com/magicliang/transaction/sys/biz/service/impl/web/advice/RestExceptionHandler.java
@@ -0,0 +1,39 @@
+package com.magicliang.transaction.sys.biz.service.impl.web.advice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+
+/**
+ * project name: leads
+ * <p>
+ * description: 解决全部的异常处理问题
+ *
+ * @author magicliang
+ * <p>
+ * date: 2022-11-17 17:10
+ */
+@Slf4j
+// 看这个注解被 @Component 注解，可以学到这个框架的一种用法
+//@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value
+            = {Exception.class})
+    protected ResponseEntity<Object> handleAllException(RuntimeException ex, HttpServletRequest req) {
+        final String uri = req.getRequestURI();
+        if (ex instanceof RuntimeException) {
+            return new ResponseEntity<>(new HashMap<String, String>(),
+                    HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(
+                "接口调用出错",
+                HttpStatus.OK);
+    }
+}
