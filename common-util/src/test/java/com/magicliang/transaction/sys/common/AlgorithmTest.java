f6d28addba14f59ab97285194dd2ea4e50c6eeb6
magicliang <magicliang@qq.com>
2023-04-11 23:52:36 +0800

    重写插入排序

--- common-util/src/test/java/com/magicliang/transaction/sys/common/AlgorithmTest.java
+++ common-util/src/test/java/com/magicliang/transaction/sys/common/AlgorithmTest.java
@@ -7,6 +7,7 @@ import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
 
@@ -158,5 +159,75 @@ public class AlgorithmTest {
             ret = ret * i;
         }
         return ret;
+    }
+
+    @Test
+    public void testInsertSort() {
+        int[] arr = new int[]{5, 3, 1, 2, 99};
+        insertSort1(arr);
+        System.out.println(Arrays.toString(arr));
+
+        arr = new int[]{5, 3, 1, 2, 99, 34, 22, 57, 48};
+        insertSort2(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    /**
+     * 不好的解法
+     * 这个解法之所以不好，是因为没有办法只做一次赋值
+     * 我们需要寻求的解应该是遍历得到一个独一无二的插入点，这个插入点的值已经被复制走了，而下一个点比它小或者等于它，不需要懂
+     *
+     * @param arr 待排序数组
+     */
+    private void insertSort1(int[] arr) {
+        int length = arr.length;
+        for (int i = 1; i < length; i++) {
+            insert1(arr, i);
+        }
+    }
+
+    private void insert1(int[] arr, int i) {
+        int key = arr[i];
+        for (int j = i - 1; j >= 0; j--) {
+            if (arr[j] <= key) {
+                arr[j + 1] = key;
+                return;
+            } else {
+                arr[j + 1] = arr[j];
+            }
+            arr[j] = key;
+        }
+    }
+
+    /**
+     * 第二种算法：比较优雅，只要求出插入点就行了，如何求出呢？
+     * <p>
+     * 1. 先正序遍历待排序数组的元素 i
+     * 2. 把它的前方当做已排序数组
+     * 3. 从后方遍历已排序数组
+     * 4. 如果后方遍历的元素 arr[j] 大于 arr[i]，不可插入，arr[j] 后退一格
+     * 5. j--
+     * 6. 如果此时的 arr[j] 小于等于arr[i]，则 arr[j+1] 适合插入，且 arr[j+1] 和 arr[j+2]在上一轮里做过了冗余
+     * 7. 如果已经遍历到尽头，则 j 已然越界， arr[j+1] 为 0。
+     * <p>
+     * 插入算法要理解好 j、j + 1、j + 2 三区间移动的关系
+     *
+     * @param arr 待排序数组
+     */
+    private void insertSort2(int[] arr) {
+        // 在每轮迭代以前，先抽取不变量
+        int length = arr.length;
+        for (int i = 1; i < length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            int tmp = arr[j];
+            // 逻辑归并以后的写法不如展开时容易理解，上一轮的状态在上一轮迭代已经完成了，每一轮的条件判定要单独做
+            while (j >= 0 && tmp > key) {
+                // 先退一位，这样，这样 a[j] 总是可插入的
+                arr[j + 1] = tmp;
+                j--;
+            }
+            arr[j + 1] = key;
+        }
     }
 }
