1e12928431f015bcfdb1c65674f26c3744b3ee6b
chuanliang <chuanliang@tencent.com>
2023-09-07 14:29:11 +0800

    增加 verify 的说明

--- common-util/src/test/java/com/magicliang/transaction/sys/common/MockitoTest.java
+++ common-util/src/test/java/com/magicliang/transaction/sys/common/MockitoTest.java
@@ -4,6 +4,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 import java.util.List;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -27,8 +28,23 @@ public class MockitoTest {
         mockedList.clear();
 
         //verification
-
-        System.out.println(verify(mockedList).add("one"));
         verify(mockedList).clear();
+        System.out.println(verify(mockedList).add("one"));
+
+        /*
+         * Wanted but not invoked:
+list.stream();
+-> at com.magicliang.transaction.sys.common.MockitoTest.testVerification(MockitoTest.java:32)
+
+However, there were exactly 2 interactions with this mock:
+list.add("one");
+-> at com.magicliang.transaction.sys.common.MockitoTest.testVerification(MockitoTest.java:26)
+
+list.clear();
+-> at com.magicliang.transaction.sys.common.MockitoTest.testVerification(MockitoTest.java:27)
+         */
+        // verify(mockedList).stream();
+
+        Assertions.assertTrue(true);
     }
 }
