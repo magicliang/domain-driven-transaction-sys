176d673eb11c48210cb075f73442a7a7d3ef3a0b
magicliang <magicliang@qq.com>
2025-07-30 23:43:49 +0800

    修复进位消失的问题

--- core-service/src/main/java/algorithm/bignumber/BigNumberCalculate.java
+++ core-service/src/main/java/algorithm/bignumber/BigNumberCalculate.java
@@ -56,16 +56,16 @@ public class BigNumberCalculate {
     }
 
     public static String add(char[] a, char[] b) {
-        // 找出两个数组中更长的作为写入的目标数组
-        // 逆序从高位取值，直到两个数组中更短的遍历完
-        // 把char转成int，然后做加法，得到结果以后如果大于10，求出余数存到 temp变量里，在下次循环以后用掉
+        // 1. 找出两个数组中更长的作为写入的目标数组
+        // 2. 逆序从高位取值，直到两个数组中更短的遍历完
+        // 3. 把char转成int，然后做加法，得到结果以后如果大于10，求出余数存到 temp变量里，在下次循环以后用掉
 
-        // 易错点：1. 是双指针同时移动，要用 while -- 的解法，而且 a、b、i、j 的搭配不能漏。如果用 temp 移动，则还需要一个额外指针。
-        //        2.  计算 extra 要知道要从 10 开始计算起，所以要 >= 10
-        //        3. extra 要提前加，清掉，再算下一个 extra，extra 是1，而不是真的余数，【因为高位只进一】
-        //        4. 终止完了以后，extra 还要再好几次，要考虑连续进位，而且只对长数组进位，而不是 temp 数组进位。
+        // 易错点：1. 是多指针同时移动，要用 while -- 的解法，而不能用嵌套 for 循环。而且 a、b、i、j 的搭配不能写错，复制粘贴最容易出错了。如果用 temp 移动，则还需要一个额外指针。不要共用指针。
+        //        2. 计算 extra 要知道要从 10 开始计算起，所以要 result >= 10，而不是 result > 10。
+        //        3. extra 要提前加，清掉，再算下一个 extra，extra 是1，而不是真的余数，【因为高位只进一】。
+        //        4. 终止完了以后，extra 还剩下，还要继续加，要考虑连续进位，继续对长数组进位，把结果写回 temp 数组。
         //        5. 用 while 来代替 if 更好
-        //        6. 最高位仍然可能越位，所以不能使用普通的 temp，最好使用更长的临时数组，而且临时数组最好先赋0，否则以后做加法取值会不均衡
+        //        6. 最高位仍然可能越位，所以不能使用普通的 temp，最好使用更长的临时数组。每次相加其实是temp比长数组更长，长数组比短数组更长，计算到后面要保证extra能够和每一个更长数组继续处理的问题。
         //        7. 前导0只去掉前导位
         if (a == null || b == null || a.length == 0 || b.length == 0) {
             throw new IllegalArgumentException("illegal argument:" + Arrays.toString(a) + "," + Arrays.toString(b));
@@ -116,21 +116,33 @@ public class BigNumberCalculate {
             j--;
         }
 
-        // 如果较短的数组遍历完了，extra还没用尽，那么继续加下去，考虑连续进位问题，
-        // 这里面有个小 tricky，temp 天然比长数组要长一个格，所以要越界是长数组先越界
-        while (extra > 0 && tempPiv - 1 >= 0) {
-            // 用长数组继续进行 extra 加法
-            char cur = a[i];
-            int result = atoi(cur);
-            result += extra;
-            extra = 0;
-            if (result >= 10) {
-                extra = 1;
-                result -= 10;
+        // 如果较短的数组遍历完了，extra还没用尽，那么继续加下去，考虑连续进位问题，只对长数组做减法
+        // 有可能 i 遍历完了，仍然要往下进一位
+        while (extra > 0) {
+            int result = 0;
+            if (i >= 0) {
+                // 能从长数组里面取值就从长数组里面取值
+                // 用长数组继续进行 extra 加法
+                char cur = a[i--];
+                result = atoi(cur);
+                result += extra;
+                extra = 0;
+                if (result >= 10) {
+                    extra = 1;
+                    result -= 10;
+                }
+
+            } else {
+                // 否则从 temp 数组取值
+                result = atoi(temp[tempPiv]);
+                result += extra;
+                extra = 0;
+                if (result >= 10) {
+                    extra = 1;
+                    result -= 10;
+                }
             }
-            temp[tempPiv] = itoa(result);
-            tempPiv--;
-            i--;
+            temp[tempPiv--] = itoa(result);
         }
 
         StringBuilder sb = new StringBuilder();
