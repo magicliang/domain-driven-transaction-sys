592fe15d86c004957ac97b88fcca6fd2dd0fbb1d
magicliang <magicliang@qq.com>
2025-07-31 00:02:25 +0800

    优化可读性

--- core-service/src/main/java/algorithm/bignumber/BigNumberCalculate.java
+++ core-service/src/main/java/algorithm/bignumber/BigNumberCalculate.java
@@ -61,9 +61,9 @@ public class BigNumberCalculate {
         // 3. 把char转成int，然后做加法，得到结果以后如果大于10，求出余数存到 temp变量里，在下次循环以后用掉
 
         // 易错点：1. 是多指针同时移动，要用 while -- 的解法，而不能用嵌套 for 循环。而且 a、b、i、j 的搭配不能写错，复制粘贴最容易出错了。如果用 temp 移动，则还需要一个额外指针。不要共用指针。
-        //        2. 计算 extra 要知道要从 10 开始计算起，所以要 result >= 10，而不是 result > 10。
-        //        3. extra 要提前加，清掉，再算下一个 extra，extra 是1，而不是真的余数，【因为高位只进一】。
-        //        4. 终止完了以后，extra 还剩下，还要继续加，要考虑连续进位，继续对长数组进位，把结果写回 temp 数组。
+        //        2. 计算 carry 要知道要从 10 开始计算起，所以要 result >= 10，而不是 result > 10。
+        //        3. carry 要提前加，清掉，再算下一个 carry，carry 是1，而不是真的余数，【因为高位只进一】。
+        //        4. 终止完了以后，carry 还剩下，还要继续加，要考虑连续进位，继续对长数组进位，把结果写回 temp 数组。
         //        5. 用 while 来代替 if 更好
         //        6. 最高位仍然可能越位，所以不能使用普通的 temp，最好使用更长的临时数组。每次相加其实是temp比长数组更长，长数组比短数组更长，计算到后面要保证extra能够和每一个更长数组继续处理的问题。
         //        7. 前导0只去掉前导位
@@ -82,13 +82,13 @@ public class BigNumberCalculate {
         int tempLength = a.length;
         char[] temp = new char[tempLength + 1];
 
-        int extra = 0;
+        int carry = 0;
 
         // 一起移动的指针不可以用嵌套for循环叉乘移动，而要在一个 while 里面
 
         int i = a.length - 1;
         int j = b.length - 1;
-        int tempPiv = temp.length - 1;
+        int k = temp.length - 1;
 
         // 默认 a 是更长的部分
         while (i >= 0 && j >= 0) {
@@ -96,42 +96,42 @@ public class BigNumberCalculate {
             final Integer bj = atoi(b[j]);
 
             // 把上一轮的用在这里
-            int result = extra + ai + bj;
+            int result = carry + ai + bj;
             // 用掉
-            extra = 0;
+            carry = 0;
 
             // 而且减法要从10开始，而不是11
             if (result >= 10) {
-                extra = 1;
+                carry = 1;
                 result -= 10;
             }
 
             // 不要忘记有3个指针要移动
-            temp[tempPiv--] = itoa(result);
+            temp[k--] = itoa(result);
             i--;
             j--;
         }
 
         // 如果较短的数组遍历完了，extra还没用尽，那么继续加下去，考虑连续进位问题，只对长数组做减法
         // 有可能 i 遍历完了，仍然要往下进一位
-        while (extra > 0 && i >= 0) {
+        while (carry > 0 && i >= 0) {
                 // 能从长数组里面取值就从长数组里面取值
-                // 用长数组继续进行 extra 加法
+            // 用长数组继续进行 carry 加法
                 char cur = a[i--];
             int result = atoi(cur);
-                result += extra;
-                extra = 0;
+            result += carry;
+            carry = 0;
                 if (result >= 10) {
-                    extra = 1;
+                    carry = 1;
                     result -= 10;
                 }
 
-            temp[tempPiv--] = itoa(result);
+            temp[k--] = itoa(result);
         }
 
         // 最后一位进位直接赋值就行，无需加法
-        if (extra > 0) {
-            temp[tempPiv--] = itoa(extra);
+        if (carry > 0) {
+            temp[k--] = itoa(carry);
         }
 
         StringBuilder sb = new StringBuilder();
