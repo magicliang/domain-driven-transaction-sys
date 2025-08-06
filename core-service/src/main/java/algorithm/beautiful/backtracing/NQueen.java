5eb9e03b718d4734aecd8dce0165be5508a783fa
chuanliang <chuanliang@tencent.com>
2025-08-06 21:54:27 +0800

    初始化一个皇后问题

---
+++ core-service/src/main/java/algorithm/beautiful/backtracing/NQueen.java
@@ -0,0 +1,18 @@
+package algorithm.beautiful.backtracing;
+
+
+/**
+ * project name: domain-driven-transaction-sys
+ *
+ * description: n皇后问题
+ * n * n 的棋盘有n平方个空间状态
+ * 这似乎是一个排列/组合问题，每一行的行肯定是错开的，问题是列在怎样的排列里是错开的
+ * 如何帮回溯剪枝？使用 used 列数组往下传
+ *
+ * @author magicliang
+ *
+ *         date: 2025-08-06 21:52
+ */
+public class NQueen {
+
+}
